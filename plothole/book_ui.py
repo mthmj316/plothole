e322eaf7517fc6406b488159c83907758fbde460
mthmj316 <64063837+mthmj316@users.noreply.github.com>
2025-12-30 16:42:08 +0100

    logging in book_ui.py integrated

--- plothole/book_ui.py
+++ plothole/book_ui.py
@@ -12,6 +12,8 @@ from tkinter import StringVar
 from tkinter import messagebox as mb
 from tkinter import scrolledtext as stxt
 from tkinter.filedialog import askopenfilename
+from inspect import currentframe
+import logger as log
 
 PAD_X_RIGHT_BOLD = (5, 20)
 PAD_X_LEFT_BOLD = (20, 5)
@@ -27,6 +29,9 @@ class BookFrame(tk.Frame, UIObservable):
     
     def __init__(self, root, *args, **kwargs):
         super().__init__(root, *args, **kwargs)
+       
+        log.log_var(self, currentframe(), ("args", args), ("kwargs", kwargs))
+        
         self.root = root
         self.observers = []
         self.default_font = tkFont.Font(family='Helvetica', size=20, weight='bold')    
@@ -92,104 +97,135 @@ class BookFrame(tk.Frame, UIObservable):
         btn_close.grid(row=0, column=4, sticky="NSEW", padx=(1,0), pady=5)
     
     def set_header(self, header):
+        log.log_var(self, currentframe(), ("header", header))
         self.lb_header.config(text=header)
     
     def close(self):
+        log.log(self, currentframe())
         self.root.close_me(self)
         for observer in self.observers:
             observer.onClose()
     
     def set_base_dir(self, base_dir):
+        log.log_var(self, currentframe(), ("base_dir", base_dir))
         self.base_dir = base_dir
 
     def _open(self):
-       # Tk().withdraw()
-       self.story_file = askopenfilename(initialdir=self.base_dir)
-       for observer in self.observers:
-           observer.onLoad(self.story_file)
+        log.log(self, currentframe())
+        self.story_file = askopenfilename(initialdir=self.base_dir)
+        for observer in self.observers:
+            observer.onLoad(self.story_file)
         
     def save(self):
+        log.log(self, currentframe())
         for observer in self.observers:
             observer.onSave()
 
     def update(self):
+        log.log(self, currentframe())
         for observer in self.observers:
             observer.onUpdate()
     
     def delete(self):
+        log.log(self, currentframe())
         for observer in self.observers:
             observer.onDelete()
         self.root.close_me(self)
 
     def revert(self):
+        log.log(self, currentframe())
         for observer in self.observers:
             observer.onRevert()
     
     def back(self):
+        log.log(self, currentframe())
         self.root.close_me(self)
     
     def raise_frame(self, abovethis):
+        log.log_var(self, currentframe(), ("abovethis", abovethis))
         self.tkraise(aboveThis=abovethis)
         for observer in self.observers:
             observer.onDisplay(self)       
 
     def get_alias(self):
+        log.log(self, currentframe())
         return self.tb_alias_value.get()
     
     def set_alias(self, alias):
+        log.log_var(self, currentframe(), ("alias", alias))
         self.tb_alias_value.set(alias)
     
     def get_title(self):
+        log.log(self, currentframe())
+        log.log(self, currentframe())
         return self.tb_title_value.get()
     
     def set_title(self, title):
+        log.log_var(self, currentframe(), ("title", title))
         self.tb_title_value.set(title)
     
     def get_accent(self):
+        log.log(self, currentframe())
         return self.tb_accent_value.get()
     
     def set_accent(self, accent):
+        log.log_var(self, currentframe(), ("accent", accent))
         self.tb_accent_value.set(accent)
   
     def get_genre(self):
+        log.log(self, currentframe())
         return self.tb_genre_value.get()
     
     def set_genre(self, genre):
-            self.tb_genre_value.set(genre)
+        log.log_var(self, currentframe(), ("genre", genre))
+        self.tb_genre_value.set(genre)
     
     def get_message(self):
+        log.log(self, currentframe())
         return self.sta_msg.get("1.0", tk.END)
     
     def set_message(self, msg):
+        log.log_var(self, currentframe(), ("msg", msg))
         self.sta_msg.delete('1.0', tk.END)
         self.sta_msg.insert(tk.INSERT, msg)
     
     def get_basic_idea(self):
+        log.log(self, currentframe())
         return self.sta_basic_idea.get("1.0", tk.END)
     
     def set_basic_idea(self, basic_idea):
+        log.log_var(self, currentframe(), ("basic_idea", basic_idea))
         self.sta_basic_idea.delete('1.0', tk.END)
         self.sta_basic_idea.insert(tk.INSERT, basic_idea)
     
     def raise_error(self, error):
+        log.log_var(self, currentframe(), ("error", error))
         mb.showerror("Fehler", error)
         
     def disable_alias(self):
+        log.log(self, currentframe())
         self.tb_alias.config(state='disabled')
         
     def enable_alias(self):
+        log.log(self, currentframe())
+        log.log(self, currentframe())
         self.tb_alias.config(state='normal')
 
     def register(self, uiobserver):
+        log.log_var(self, currentframe(), ("uiobserver", uiobserver))
         self.observers.append(uiobserver)
         
     def unregister(self, uiobserver):
+        log.log_var(self, currentframe(), ("uiobserver", uiobserver))
         self.observers.pop(self.observers.index(uiobserver))
 
 class BookOverviewFrame(tk.Frame, UIObservable):
     
     def __init__(self, root, *args, **kwargs):
         super().__init__(root, *args, **kwargs)
+       
+        log.log_var(self, currentframe(), ("args", args), ("kwargs", kwargs))
+        
         self.grid_columnconfigure(0, weight=1)
         self.next_button_row = 0
         self.root = root
@@ -209,34 +245,42 @@ class BookOverviewFrame(tk.Frame, UIObservable):
         self.next_button_row += 1
   
     def back(self):
+        log.log(self, currentframe())
         self.root.close_me(self)
     
     def new_book(self):
+        log.log(self, currentframe())
         self.root.close_me(self, back=False)
     
     def raise_frame(self, abovethis):
+        log.log_var(self, currentframe(), ("abovethis", abovethis))
         self.tkraise(aboveThis=abovethis)
         for observer in self.observers:
             observer.onDisplay(self)      
 
     def register(self, uiobserver):
+        log.log_var(self, currentframe(), ("uiobserver", uiobserver))
         self.observers.append(uiobserver)
         
     def unregister(self, uiobserver):
+        log.log_var(self, currentframe(), ("uiobserver", uiobserver))
         self.observers.pop(self.observers.index(uiobserver))
     
     def on_book_select(self, alias):
+        log.log_var(self, currentframe(), ("alias", alias))
         self.root.close_me(self, back=False)
         for observer in self.observers:
             observer.onSelect(alias, PlotHoleType.BOOK)
     
-    def remove_all_book_buttons(self):        
+    def remove_all_book_buttons(self):
+        log.log(self, currentframe())        
         for btn in self.book_buttons:
             btn.destroy()            
         self.next_button_row = 1
         self.book_buttons.clear()
         
-    def create_book_button(self, alias):        
+    def create_book_button(self, alias):   
+        log.log_var(self, currentframe(), ("alias", alias))     
         btn = tk.Button(self, text=alias, command=lambda: self.on_book_select(alias))
         btn.grid(row=self.next_button_row, column=0, columnspan=2, sticky="NSEW", padx=(5,5), pady=(5,5))
         btn['font'] = tkFont.Font(family='Helvetica', size=20, weight='bold')
@@ -245,4 +289,5 @@ class BookOverviewFrame(tk.Frame, UIObservable):
         self.book_buttons.append(btn)
         
     def set_header(self, header):
+        log.log_var(self, currentframe(), ("header", header))
         self.lb_header.config(text=header)
