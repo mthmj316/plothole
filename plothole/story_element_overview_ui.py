9289766aee0189e2f78fdb9fd5d965c54fe627c5
mthmj316 <64063837+mthmj316@users.noreply.github.com>
2026-01-16 11:18:45 +0100

    chapter ui finished

--- plothole/story_element_overview_ui.py
+++ plothole/story_element_overview_ui.py
@@ -313,6 +313,60 @@ class StoryOverviewFrame(StoryElementOverviewFrame, UIObservable, NavigationPoin
         
         return conf
 
+class ChapterOverviewFrame(StoryElementOverviewFrame, UIObservable, NavigationPoint):
+    def __init__(self, root, *args, **kwargs):
+        super().__init__(root, self.create_conf(), PlotHoleType.CHAPTER, *args, **kwargs)
+
+    def create_conf(self):
+        
+        conf = __SEConfiguration__()
+        conf.set_column_weigth(3, 1)
+        conf.set_column_weigth(5, 1)
+        conf.set_row_weigth(3, 1)
+        conf.set_grid_column_ctn(6)
+        conf.set_grid_row_ctn(5)
+        
+        conf.set_label_colspan(__SEControls__.HEADER, 7)
+        conf.set_label(__SEControls__.HEADER,'Kapitel Übersicht')
+        conf.set_label_position(__SEControls__.HEADER, (0,0))
+        conf.set_label_sticky(__SEControls__.HEADER, tk.W)
+        conf.set_label_font(__SEControls__.HEADER, tkFont.Font(family='Helvetica', size=15, weight=tkFont.BOLD))
+        conf.set_label_anchor(__SEControls__.HEADER, tk.W)
+        
+        btn_width = 15
+        
+        secontrol = __SEControls__.BTN_CHARACTER 
+        conf.set_control_position(secontrol, (3,0))
+        conf.set_label(secontrol,'Charakter')
+        conf.set_control_padx(secontrol, (1,1))
+        conf.set_button_width(secontrol, btn_width)
+    
+        secontrol = __SEControls__.BTN_NEW
+        conf.set_control_position(secontrol, (0,0))
+        conf.set_label(secontrol,'Neu')
+        conf.set_control_padx(secontrol, (20,1))
+        conf.set_button_width(secontrol, btn_width)
+        
+        secontrol = __SEControls__.BTN_CLOSE
+        conf.set_control_position(secontrol, (1,0))
+        conf.set_label(secontrol,'Schließen')
+        conf.set_control_padx(secontrol, (1,1))
+        conf.set_button_width(secontrol, btn_width)
+        
+        secontrol = __SEControls__.BTN_PLOTHOLE
+        conf.set_control_position(secontrol, (2,0))
+        conf.set_label(secontrol,'Plothole')
+        conf.set_control_padx(secontrol, (1,1))
+        conf.set_button_width(secontrol, btn_width)
+        
+        secontrol = __SEControls__.BTN_TOP
+        conf.set_control_position(secontrol, (4,0))
+        conf.set_label(secontrol,'^')
+        conf.set_control_padx(secontrol, (1,1))
+        conf.set_button_width(secontrol, 5)
+        
+        return conf
+
 class PlotholeOverviewFrame(StoryElementOverviewFrame, UIObservable, NavigationPoint):
     def __init__(self, root, *args, **kwargs):
         super().__init__(root, self.create_plothole_conf(), PlotHoleType.PLOTHOLE, *args, **kwargs)
@@ -365,7 +419,7 @@ if __name__ == '__main__':
     w.grid_rowconfigure(0, weight=1)
     
     # frame = PartOverviewFrame(w)
-    frame = PlotholeOverviewFrame(w)
+    frame = ChapterOverviewFrame(w)
     frame.grid(row=0, column=0, sticky="NSEW")
     
     frame.add_overview_item("test_id", 'Test Eintrag')
