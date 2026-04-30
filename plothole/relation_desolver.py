23a4edb1d7cdc14da1c3271e09e28aff323e1d4a
mthmj316 <64063837+mthmj316@users.noreply.github.com>
2026-04-30 11:30:41 +0200

    relation_desolver.py implementation finished

--- plothole/relation_desolver.py
+++ plothole/relation_desolver.py
@@ -11,21 +11,77 @@ import helpers as hlp
 
 import pathlib
 import plothole_types as pt
+from story_element_ui import __SEControls__ as sec
 
 
 def desolve_by_path(path, return_path_only=False):
-    pass
+    
+    log.log_var(None, currentframe(), ('path',path), ('return_path_only',return_path_only))
+    
+    relations = {}
+    
+    relations['this'] = hlp.get(path, as_dict=True)
+    relations['this_ptype'] = __get_ptype_by_path(path)
+    relations['this_path'] = path
+    
+    while True:
+        
+        parent_ptype = __get_parent_ptype_by_path(path)
+        
+        if parent_ptype is None:
+            break
+        
+        parent_obj = desolve_parent_by_path(path)
+        log.log_var(None, currentframe(), ('parent_obj',parent_obj))
+        
+        parent_alias = parent_obj.get(sec.ALIAS)
+        log.log_var(None, currentframe(), ('parent_alias',parent_alias))
+        
+        parent_path = hlp.get_path_for_alias(__get_parent_path(path), parent_alias, extension=parent_ptype)
+        log.log_var(None, currentframe(), ('parent_path',parent_path))
+        
+        parent_obj['path'] = parent_path
+        parent_obj['pytpe'] = parent_ptype
+        relations[parent_ptype] = parent_obj        
+        
+        path = parent_path
+        
+    log.log_var(None, currentframe(), ("relations", relations))
 
+    return relations    
+
+
+def __get_ptype_by_path(path):
+
+    log.log_var(None, currentframe(), ("path", path))
+    
+    ptype = pathlib.Path(path).suffix.split(".")[-1]
+
+    log.log_var(None, currentframe(), ("ptype", ptype))
+
+    return ptype    
+
+def __get_parent_path(child_path):
+
+    log.log_var(None, currentframe(), ("child_path", child_path))
+    
+    parent_path = pathlib.Path(pathlib.Path(child_path).parent).parent
+   
+    log.log_var(None, currentframe(), ("parent_path", parent_path)) 
+    
+    return parent_path
+    
+    
 
 def desolve_parent_by_path(path, return_path_only=False):
     
     log.log_var(None, currentframe(), ('path',path), ('return_path_only',return_path_only))
     
     # folder of the story element parent: part -> book
-    parent_folder = pathlib.Path(pathlib.Path(path).parent).parent
+    parent_folder = __get_parent_path(path)
     log.log_var(None, currentframe(), ("parent_folder", parent_folder))
     
-    parent_ptype = pt.PARENT_PLOTHOLE_TYPE.get(pathlib.Path(path).suffix.split(".")[-1])
+    parent_ptype = __get_parent_ptype_by_path(path)
     log.log_var(None, currentframe(), ("parent_ptype", parent_ptype))
     
     parent_se = None
@@ -42,20 +98,40 @@ def desolve_children_by_path(path, return_path_only=False):
     
     log.log_var(None, currentframe(), ('path',path), ('return_path_only',return_path_only))
     
-    parent_folder = pathlib.Path(path).parent
-    log.log_var(None, currentframe(), ("parent_folder", parent_folder))
+    folder = pathlib.Path(path).parent
+    log.log_var(None, currentframe(), ("folder", folder))
 
-    child_ptype = pt.CHILD_PLOTHOLE_TYPE.get(pathlib.Path(path).suffix.split(".")[-1])
+    child_ptype = __get_child_ptype_by_path(path)
     log.log_var(None, currentframe(), ("child_ptype", child_ptype))
     
     children_se = None
     
     if child_ptype is not None:
-        children_se = hlp.get_all(parent_folder, child_ptype, as_dict=True)
+        children_se = hlp.get_all(folder, child_ptype, as_dict=True)
     
     log.log_var(None, currentframe(), ("children_se", children_se))
     
-    return children_se 
+    return children_se
+
+def __get_parent_ptype_by_path(path):
+
+    log.log_var(None, currentframe(), ("path", path))
+    
+    ptype = pt.PARENT_PLOTHOLE_TYPE.get(__get_ptype_by_path(path))
+
+    log.log_var(None, currentframe(), ("ptype", ptype))
+
+    return ptype   
+
+def __get_child_ptype_by_path(path):
+
+    log.log_var(None, currentframe(), ("path", path))
+    
+    ptype = pt.CHILD_PLOTHOLE_TYPE.get(__get_ptype_by_path(path))
+
+    log.log_var(None, currentframe(), ("ptype", ptype))
+
+    return ptype   
 
 if __name__ == '__main__':
     
@@ -68,5 +144,8 @@ if __name__ == '__main__':
     log.log_var(None, currentframe(), ("result_desolve_parent_by_path", result_desolve_parent_by_path))
     
     result_desolve_children_by_path = desolve_children_by_path(test_path)   
-    log.log_var(None, currentframe(), ("result_desolve_children_by_path", result_desolve_children_by_path))    
+    log.log_var(None, currentframe(), ("result_desolve_children_by_path", result_desolve_children_by_path))   
+    
+    result_relations = desolve_by_path(test_path)   
+    log.log_var(None, currentframe(), ("result_relations", result_relations))    
     
